67f15af2fda64f6b09a3b24fd43f5e99ddfcaf8b
mikwitz <mikaylahowitz@gmail.com>
2021-11-02 16:46:49 -0600

    created the shop

--- assets/js/game.js
+++ assets/js/game.js
@@ -59,8 +59,15 @@ var startGame = function() {
             window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
             var pickedEnemyName = enemyNames[i];
             enemyHealth = 50;
-            //debugger;
+            debugger;
             fight(pickedEnemyName);
+            if (playerHealth > 0 && i < enemyNames.length - 1) {
+                var storeConfirm = window.confirm("The fight is over, visit the shop before the next round?");
+                if(storeConfirm) {
+                    shop();
+                }
+                
+            }
         }
         else {
             window.alert("You have lost your robot in battle! Game Over!");
@@ -85,6 +92,42 @@ var endGame = function() {
 
 
 };
+
+var shop = function() {
+    var shopOption = window.prompt(
+        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+    );
+    switch(shopOption) {
+        case "REFILL" :
+        case "refill" :
+            if (playerMoney > 7) {
+            window.alert("Refilling player's health by 20 for 7 dollars");
+            playerHealth = playerHealth + 20;
+            playerMoney = playerMoney - 7;
+            } else {
+                window.alert("You don't have enough money!");
+            }
+            break;
+        case "UPGRADE":
+        case "upgrade":
+            if (playerMoney > 7) {
+                window.alert("Upgrading player's attack by 6 for 7 dollars");
+                playerAttack = playerAttack + 6;
+                playerMoney = playerMoney-7;
+            } else {
+                window.alert("You don't have enoug money!");
+            }
+            break;
+        case "LEAVE":
+        case "leave":
+            window.alert("Leaving the store");
+            break;
+        default:
+            window.alert("You did not pick a valid option. Please try again.");
+            shop();
+            break;
+    }
+}
 
 startGame();
 
