8f03fc86308328a88d33969361260097b2e71d1f
mikwitz <mikaylahowitz@gmail.com>
2021-11-04 17:16:34 -0600

    added integers to shop input

--- assets/js/game.js
+++ assets/js/game.js
@@ -118,7 +118,7 @@ var startGame = function () {
     for (var i = 0; i < enemyInfo.length; i++) {
         if (playerInfo.health > 0) {
             window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
-            debugger;
+            //debugger;
             var pickedEnemyObj = enemyInfo[i];
             pickedEnemyObj.health = randomNumber(60, 40);
             fight(pickedEnemyObj);
@@ -154,21 +154,19 @@ var endGame = function () {
 
 };
 
-var shop = function () {
+var shop = function() {
     var shopOption = window.prompt(
-        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE to make a choice."
     );
+    shopOption = parseInt(shopOption);
     switch (shopOption) {
-        case "REFILL":
-        case "refill":
+        case 1:
             playerInfo.refillHealth();
             break;
-        case "UPGRADE":
-        case "upgrade":
+        case 2:
             playerInfo.upgradeAttack();
             break;
-        case "LEAVE":
-        case "leave":
+        case 3:
             window.alert("Leaving the store");
             break;
         default:
